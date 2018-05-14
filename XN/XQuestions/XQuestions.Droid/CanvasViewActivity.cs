fe5eeb7dddb6282f08bcfde58e148d075a20a350
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2018-05-14 14:11:43 +0900

    change icon file

--- XN/XQuestions/XQuestions.Droid/CanvasViewActivity.cs
+++ XN/XQuestions/XQuestions.Droid/CanvasViewActivity.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.App;
-using Android.Widget;
-using Android.OS;
-using Android.Graphics;
-using Android.Views;
+﻿using Android.App;
 using Android.Content;
-using Android.Util;
+using Android.Graphics;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
 
 namespace XQuestions.Droid
 {
